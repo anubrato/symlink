a39b224a86fd0045fecdcb8828dbdd062aff3d85
Anubrato Roy <anubrato.roy@misys.com>
2017-04-21 19:06:27 +0530

    Add cli arg to add local module prefix. Use strict version resolution for dependencies

--- bin.js
+++ bin.js
@@ -2,8 +2,8 @@
 var argv = require('yargs')
   .usage('Usage: symlink <repoDirs> [options]')
   .demand(1)
-  .example('symlink ./repos')
-  .example('symlink ./repos -g coveralls -g nodeunit')
+  .example('symlink ./repos -p @mycompany')
+  .example('symlink ./repos -g coveralls -g nodeunit -p @mycompany')
   .alias('e', 'execute')
   .describe('e', 'Execute the commands generated')
   .boolean('e')
@@ -12,6 +12,10 @@ var argv = require('yargs')
   .array('g') // NB: will be unset if not used (default strings are ugly)
   .alias('h', 'help')
   .help('h')
+  .alias('p', 'prefix')
+  .describe('p', 'Prefix for local modules')
+  .demandOption(['p'], 'Please provide a local module prefix to handle peerDependencies correctly.')
+  .array('p') // NB: Like 'g', will be unset if not used
   .argv;
 
 require('./').cli(argv, function (err) {
